d93d7cdeb06d905741da581bad6f7b30a9b4b5f1
Sherry He <xsherryhe@gmail.com>
2022-11-20 14:37:58 -0500

    Refactored validation code to use build-in JS constraint validation API

--- scripts/books.js
+++ scripts/books.js
@@ -4,25 +4,34 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
   this.read = !this.read;
-}
+};
 
 const library = [new Book('Les Misérables', 'Victor Hugo', 1462, true)];
 
+const newBookButton = document.querySelector('.new-book-button');
+const newBookForm = document.querySelector('.new-book-form');
+const inputs = ['title', 'author', 'pages', 'read'].map((attribute) =>
+  document.getElementById(attribute)
+);
+const newBookFormSubmit = document.querySelector('.submit');
+const booksContainer = document.querySelector('.books');
+
 function addBook(e) {
+  if (!validate()) return;
   e.preventDefault();
-  const inputs = ['title', 'author', 'pages', 'read']
-                 .map(attribute => document.getElementById(attribute));
-  if(!validateInputs(...inputs)) return;
-  
-  const attributes = inputs.slice(0, 3).map(input => input.value).concat(inputs[3].checked),
-        book = new Book(...attributes);
+
+  const attributes = inputs
+      .slice(0, 3)
+      .map((input) => input.value)
+      .concat(inputs[3].checked),
+    book = new Book(...attributes);
   library.push(book);
   displayBook(book, library.indexOf(book));
-  document.querySelector('.new-book-form').reset();
+  form.reset();
 }
-document.querySelector('.submit').addEventListener('click', addBook);
+newBookFormSubmit.addEventListener('click', addBook);
 
 function removeBook(e) {
   const index = e.target.parentNode.dataset.index;
@@ -32,49 +41,57 @@ function removeBook(e) {
 
 function toggleBookRead(e) {
   const readElement = e.target.parentNode,
-        readText = readElement.querySelector('.read-text'),
-        index = readElement.parentNode.parentNode.dataset.index,
-        book = library[index];
+    readText = readElement.querySelector('.read-text'),
+    index = readElement.parentNode.parentNode.dataset.index,
+    book = library[index];
   book.toggleRead();
   e.target.textContent = `Mark as ${book.read ? 'Unread' : 'Read'}`;
   readText.classList.toggle('marked-as-read');
-  readText.textContent = `You have ${book.read ? 'read' : 'not read'} this book.`;
+  readText.textContent = `You have ${
+    book.read ? 'read' : 'not read'
+  } this book.`;
 }
 
 function displayBook(book, index) {
   const bookElement = createBookElement(index);
-  [createBookDetailsElement(book), createRemoveElement()]
-  .forEach(element => bookElement.appendChild(element));
-  document.querySelector('.books').prepend(bookElement);
+  [createBookDetailsElement(book), createRemoveElement()].forEach((element) =>
+    bookElement.appendChild(element)
+  );
+  booksContainer.prepend(bookElement);
 }
 
 function displayBooks() {
-  document.querySelector('.books').textContent = '';
+  booksContainer.textContent = '';
   library.forEach((book, i) => displayBook(book, i));
 }
 displayBooks();
 
 function toggleForm() {
-  document.querySelector('.new-book-form').classList.toggle('hidden');
+  newBookForm.classList.toggle('hidden');
 }
-document.querySelector('.new-book-button').addEventListener('click', toggleForm);
+newBookButton.addEventListener('click', toggleForm);
 
-function validateInputs(title, author, pages, read) {
-  document.querySelectorAll('.error').forEach(error => error.remove());
-  [title, author, pages, read].forEach(element => element.classList.remove('invalid'));
+function validate() {
+  document.querySelectorAll('.error').forEach((error) => error.remove());
 
-  let valid = true;
-  [title, author].forEach(element => {
-    if(element.value == '') {
-      element.classList.add('invalid');
-      const error = document.createElement('div');
-      error.classList.add('error');
-      error.textContent = `${capitalize(element.name)} cannot be blank`;
-      element.insertAdjacentElement('afterend', error);
-      valid = false;
-    }
-  })
-  return valid;
+  for (const input of inputs) {
+    input.classList.remove('submit-invalid');
+    if (input.checkValidity()) continue;
+
+    input.classList.add('submit-invalid');
+    const error = document.createElement('div');
+    error.classList.add('error');
+    input.insertAdjacentElement('afterend', error);
+
+    if (input.validity.valueMissing)
+      error.textContent = `${capitalize(input.name)} cannot be blank`;
+
+    if (input.validity.rangeUnderflow)
+      error.textContent += `${capitalize(input.name)} cannot be less than ${
+        input.minLength
+      }`;
+  }
+  return form.checkValidity();
 }
 
 function capitalize(string) {
@@ -90,9 +107,12 @@ function createBookElement(index) {
 
 function createBookDetailsElement(book) {
   const bookDetailsElement = document.createElement('div');
-  [createTitleElement(book.title), createAuthorElement(book.author),
-   createPagesElement(book.pages), createReadElement(book.read)]
-  .forEach(element => bookDetailsElement.appendChild(element));
+  [
+    createTitleElement(book.title),
+    createAuthorElement(book.author),
+    createPagesElement(book.pages),
+    createReadElement(book.read),
+  ].forEach((element) => bookDetailsElement.appendChild(element));
   return bookDetailsElement;
 }
 
@@ -119,14 +139,18 @@ function createPagesElement(pagesContent) {
 
 function createReadElement(readBoolean) {
   const readElement = document.createElement('div'),
-        readText = document.createElement('p'),
-        toggleReadButton = document.createElement('button');
+    readText = document.createElement('p'),
+    toggleReadButton = document.createElement('button');
   readElement.classList.add('read');
   readText.classList.add('read-text', readBoolean ? 'marked-as-read' : null);
-  readText.textContent = `You have ${readBoolean ? 'read' : 'not read'} this book.`;
+  readText.textContent = `You have ${
+    readBoolean ? 'read' : 'not read'
+  } this book.`;
   toggleReadButton.textContent = `Mark as ${readBoolean ? 'Unread' : 'Read'}`;
   toggleReadButton.addEventListener('click', toggleBookRead);
-  [readText, toggleReadButton].forEach(element => readElement.appendChild(element));
+  [readText, toggleReadButton].forEach((element) =>
+    readElement.appendChild(element)
+  );
   return readElement;
 }
 
